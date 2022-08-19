6cf2f46c429e4b770994200ac1dac13c8c56e631
Davincii254 <victorkuria7@gmail.com>
2022-08-19 07:30:06 +0300

    Update

--- db/migrate/20211216194618_remove_failures_column_and_date_column.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFailuresColumnAndDateColumn < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :launches, :failure_notes
-    remove_column :launches, :date_of_launch
-  end
-end
